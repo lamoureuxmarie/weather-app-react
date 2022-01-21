3ce5c1ffe47de30cc937dbc3d53558079808079e
Marie Lamoureux <69575238+lamoureuxmarie@users.noreply.github.com>
2022-01-21 20:02:08 +0100

    added axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 
 export default function Weather() {
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    const apiKey = "666e992c8bf5317be35ba26eb820d6ec";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse); 
+
+    function handleResponse(response) {
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            temperature: response.data.main.temp,
+            humidity: response.data.main.humidity,
+            description: response.data.weather[0].description,
+            iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+            icon: response.data.weather[0].icon,
+            wind: response.data.wind.speed,
+            city: response.data.name,
+            date: new Date(response.data.dt * 1000)
+        })
+    }
+
+    if (weatherData.ready) {
     return <div className="Weather">
 
-        <h1>Berlin</h1>
+        <h1 className="city">{weatherData.city}</h1> 
         <ul>
-            <li>Wednesday 07:00</li>
-            <li>Mostly Cloudy</li>
+            <li>{weatherData.date}</li>
+            <li className="text-capitalize">{weatherData.city}</li>
         </ul>
 
-        <div className="row">
+        <div className="row mt-3">
           <div className="col-6">
-            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny"/>
-            6°C
+          <div className="clearfix">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny"className="float-left" />
+            <div className="float-left">
+            <span className="temperature">{Math.round(weatherData.temperature)}</span><span className="unit">°C</span>
+            </div>
+            </div>
           </div>
 
 
         <div className="col-6">
         <ul>
-            <li>Précipitations : 1%</li>
-            <li>Humidité : 62%</li>
-            <li>Vent : 23 km/h</li>
+            <li>Humidité : {weatherData.humidity}%</li>
+            <li>Vent : {weatherData.wind} km/h</li>
         </ul>
         </div>
         </div>
@@ -38,11 +63,12 @@ export default function Weather() {
               />
             </div>
             <div className="col-6">
-            <button className="btn btn-primary" type="submit">Go</button>
-            <button className="btn btn-primary">Current location</button>
+            <button className="btn btn-primary" Type="submit">Go</button>
+            <button className="btn btn-primary" Type="submit">Current location</button>
             </div>
             </div>
             </form>
 
             </div>
+}
 }
