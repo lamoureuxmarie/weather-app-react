9c42a73aea530c2c1452c71065f42df1c3b34a5c
Marie Lamoureux <69575238+lamoureuxmarie@users.noreply.github.com>
2022-02-06 19:51:03 +0100

    implemented dark/light mode toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import React, { useState } from "react";
 import Weather from './Weather';
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./Theme.js";
+import Toggle from './Toggle';
+
+const StyledApp = styled.div`
+  color: ${(props) => props.theme.fontColor};
+`;
 
 function App() {
+  const [theme, setTheme] = useState(() => false);
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+    <StyledApp>
     <div className="App">
+    <button onClick={() => themeToggler()}><Toggle /></button>
       <Weather defaultCity="Berlin" />
     </div>
+    </StyledApp>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Toggle.js
@@ -0,0 +1,14 @@
+
+import React, {useState} from "react";
+import DarkModeToggle from "react-dark-mode-toggle";
+
+export default function Toggle () {
+  const [isDarkMode, setIsDarkMode] = useState(() => false);
+  return (
+    <DarkModeToggle
+      onChange={setIsDarkMode}
+      checked={isDarkMode}
+      size={60}
+    />
+  );
+};
