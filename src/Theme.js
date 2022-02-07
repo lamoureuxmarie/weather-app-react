75e138ea4087b23fa66bad2d8a1ce208cd842953
Marie Lamoureux <69575238+lamoureuxmarie@users.noreply.github.com>
2022-02-07 15:04:56 +0100

    added background theme images

--- src/Theme.js
+++ src/Theme.js
@@ -3,11 +3,13 @@ import { createGlobalStyle } from "styled-components";
 export const lightTheme = {
   body: "#fff",
   fontColor: "#000",
+  image: "https://images.unsplash.com/photo-1608657803991-b62ca5de7a12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1925&q=80",
 };
 
 export const darkTheme = {
   body: "#181818",
-  fontColor: "#fff",
+  fontColor: "#FAFAFA",
+  image: "https://images.unsplash.com/photo-1516166328576-82e16a127024?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
 };
 
 export const GlobalStyles = createGlobalStyle`
@@ -15,4 +17,9 @@ export const GlobalStyles = createGlobalStyle`
 		background-color: ${(props) => props.theme.body};
     transition: all 0.25s linear;
 	}
+  body {
+    background-image: url(${(props) => props.theme.image});
+  }
+
+
 `;
